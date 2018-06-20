abb68d9048e0a3679dffcddf4cbc5e0dfd6c02a0
Anu Khambete <anukhambete@gmail.com>
2018-06-20 20:20:11 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
-class AddFavoriteFoodToArtists < ActiveRecord::Migration[5.1]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
     add_column :artists, :grade, :integer
     add_column :artists, :birthdate, :string
